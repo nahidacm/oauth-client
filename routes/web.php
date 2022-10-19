cd7874a037e5804a498be1a1dafb8a72d7ae22ab
Mahmudur Rahman <nahidacm@gmail.com>
2022-10-19 12:02:39 +0600

    code clean

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Http;
-use InvalidArgumentException;
 
 /*
 |--------------------------------------------------------------------------
